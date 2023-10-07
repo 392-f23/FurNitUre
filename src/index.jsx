e7d203e086e862b707907a6b3f22d5670fba513a
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2023-10-06 22:24:33 -0500

    fix some css issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import App from "./App";
 import "./index.less";
+
+const theme = createTheme({
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: false,
+      },
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
