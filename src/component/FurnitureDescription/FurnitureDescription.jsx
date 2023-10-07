7a7de27f7a510161ec2eb21476c0ea245968e50f
chzhang981 <edwardzhangcongkai@gmail.com>
2023-10-07 17:53:44 -0500

    Furniture Des. CSS Modify

--- src/component/FurnitureDescription/FurnitureDescription.jsx
+++ src/component/FurnitureDescription/FurnitureDescription.jsx
@@ -11,25 +11,26 @@ const ButtonBar = () => {
 };
 
 const FurnitureDescription = () => {
-    return (
-        <div className="furniture-description">
-            <img src="https://cdn.jsdelivr.net/gh/Hongda-OSU/PicGo/image/table.jpg" alt="Table" className="furniture-image" />
-            <div className="furniture-details">
-                <p>Name: Furniture</p>
-                <p>Price: $40</p>
-                <p>Description: Discover the essence of elegant living with this exquisitely
-                  handcrafted table, blending modern design with traditional
-                  craftsmanship. Its sleek lines and robust wooden structure infuse a
-                  touch of unique charm into your living space. Every detail is
-                  meticulously refined, ensuring it's not just a practical piece of
-                  furniture but an artistic statement. Moreover, its multifunctional
-                  design makes it suitable for various settings, be it a formal office
-                  environment or a cozy home ambiance. Choose our table and elevate
-                  your space with distinctive style.</p>
-            </div>
-            <ButtonBar />
-        </div>
-    )
+  return (
+      <div className="furniture-description">
+          <img src="https://cdn.jsdelivr.net/gh/Hongda-OSU/PicGo/image/table.jpg" alt="Table" className="furniture-image" />
+          <div className="furniture-details">
+              <p><span className="name-label">Name:</span> Furniture</p>
+              <p><span className="price-label">Price:</span> $40</p>
+              <p><span className="description-label">Description:</span> Discover the essence of elegant living with this exquisitely
+                handcrafted table, blending modern design with traditional
+                craftsmanship. Its sleek lines and robust wooden structure infuse a
+                touch of unique charm into your living space. Every detail is
+                meticulously refined, ensuring it's not just a practical piece of
+                furniture but an artistic statement. Moreover, its multifunctional
+                design makes it suitable for various settings, be it a formal office
+                environment or a cozy home ambiance. Choose our table and elevate
+                your space with distinctive style.</p>
+          </div>
+          <ButtonBar />
+      </div>
+  )
 };
+
 
 export default FurnitureDescription;
