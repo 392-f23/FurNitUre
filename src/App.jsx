d69bfc5d33bd021d28f473087f1d959ed4f72f86
chzhang981 <edwardzhangcongkai@gmail.com>
2023-10-12 11:35:23 -0500

    compon&des-json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.less";
 import Dispatcher from "./component/Dispatcher";
+import data from './component/mockData.json';
 
 const App = () => {
   return (
     <div className="app">
-      <Dispatcher />
+      <Dispatcher data={data}/>
     </div>
   );
 };
--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -1,11 +1,12 @@
 import FurnitureHeader from "./FurnitureHeader/FurnitureHeader";
 import FurnitureContent from ".//FurnitureContent/FurnitureContent";
 
-const MainPage = () => {
+const MainPage = (props) => {
+  const {data} = props
   return (
     <div className="main">
       <FurnitureHeader />
-      <FurnitureContent />
+      <FurnitureContent data={data} />
     </div>
   );
 };
