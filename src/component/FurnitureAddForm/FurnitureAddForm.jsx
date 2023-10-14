a91f23bffd2668b8e483f88d644a9d1ac12b4cf1
Adrian Hoffer <adrianhoffer35@gmail.com>
2023-10-14 15:15:28 -0500

    added buttons to add more features, submit handler

--- src/component/FurnitureAddForm/FurnitureAddForm.jsx
+++ src/component/FurnitureAddForm/FurnitureAddForm.jsx
@@ -11,17 +11,92 @@ import { useNavigate } from "react-router-dom";
 import furniturePlaceholder from "./furniturePlaceHolder.png";
 import "./FurnitureAddForm.less";
 
+const initialFormState = {
+  Item: "",
+  Description: "",
+  Price: "",
+  Location: "",
+  DeliveryMethod: "",
+  Condition: "",
+  Material: "",
+  Finish: "",
+};
+
+const InputField = ({ name, text, state, change }) => (
+  <div className="mb-3">
+    <label htmlFor={name} className="form-label">
+      {text}
+    </label>
+    <input
+      className="form-control"
+      id={name}
+      name={name}
+      value={state[name]} 
+      onChange={(e) => change({ ...state, [name]: e.target.value })}
+
+    />
+    <div className="invalid-feedback">{state.errors?.[name]}</div>
+  </div>
+);
+
+const ButtonBar = ({ message, disabled }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="bar">
+      <button type="button" className="btn btn-outline-dark mx-auto  me-2" onClick={() => navigate(-1)}>
+        Cancel
+      </button>
+      <button type="submit" className="btn btn-primary me-auto" disabled={disabled}>
+        Submit
+      </button>
+      <span className="p-2">{message}</span>
+    </div>
+  );
+};
+
 const AddForm = ({ addFurniture }) => {
   const navigate = useNavigate();
+  const [update, result] = useState("");
   const [focused, setFocused] = useState(null);
+  const [state, change] = useState(initialFormState);
+  const [showInputFields, setShowInputFields] = useState({
+    DeliveryMethod: false,
+    Condition: false,
+    Material: false,
+    Finish: false,
+  });
 
-  const handleFocus = (inputName) => {
-    setFocused(inputName);
+  const toggleInputFields = (field) => {
+    setShowInputFields((prevState) => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    const formData = {
+      item: state.Item,
+      image: null,
+      price: state.Price,
+      description: state.Description,
+      
+    };
+/*    Location: state.Location,
+      DeliveryMethod: showInputFields.DeliveryMethod ? state.DeliveryMethod : null,
+      Condition: showInputFields.Condition ? state.Condition : null,
+      Material: showInputFields.Material ? state.Material : null,
+      Finish: showInputFields.Finish ? state.Finish : null, */
+    console.log(formData);
+    addFurniture(formData);
+    navigate(-1);
+    //alert("Submitted");
   };
 
   return (
     <>
       <div className="topPad"></div>
+    
       <Card
         variant="outlined"
         sx={{
@@ -61,45 +136,43 @@ const AddForm = ({ addFurniture }) => {
           height="500"
           image={furniturePlaceholder}
         />
-        <CardContent>
-          <Typography variant="body1" color="textPrimary">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              Add a Description:
-              <input
-                type="text"
-                name="Description"
-                placeholder="Describe your product..."
-                onFocus={() => handleFocus("title")}
-              />
-            </div>
-          </Typography>
-        </CardContent>
-        <CardContent>
-          <Typography variant="body1" component="div" color="Primary">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              Add a price:
-              <input
-                type="text"
-                name="title"
-                placeholder="Price"
-                onFocus={() => handleFocus("title")}
-              />
-            </div>
-          </Typography>
-        </CardContent>
-        <Button
-          variant="outlined"
-          onClick={() => navigate("/")}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "0 auto",
-            marginBottom: "10px",
-          }}
-        >
-          Post
-        </Button>
+
+        <form onSubmit={submit} noValidate className={state.errors ? "was-validated" : null}>
+          <InputField name="Item" text="Item" state={state} change={change} />
+          <InputField name="Description" text="Description" state={state} change={change} />
+          <InputField name="Price" text="Price" state={state} change={change} />
+          <InputField name="Location" text="Location" state={state} change={change} />
+          {showInputFields.DeliveryMethod && (
+            <InputField name="DeliveryMethod" text="Delivery Method" state={state} change={change} />
+          )}
+          {showInputFields.Condition && (
+            <InputField name="Condition" text="Condition" state={state} change={change} />
+          )}
+          {showInputFields.Material && (
+            <InputField name="Material" text="Material" state={state} change={change} />
+          )}
+          {showInputFields.Finish && (
+            <InputField name="Finish" text="Finish" state={state} change={change} />
+          )}
+          <div className="bar">
+            Add More Information:
+          <button className="mx-auto " type="button"
+            onClick={() => toggleInputFields("DeliveryMethod")}
+          >
+            Delivery Method
+          </button>
+          <button className="mx-auto" type="button" onClick={() => toggleInputFields("Condition")}>
+            Condition
+          </button>
+          <button className="mx-auto" type="button" onClick={() => toggleInputFields("Material")}>
+            Material
+          </button>
+          <button  className="mx-auto"type="button" onClick={() => toggleInputFields("Finish")}>
+            Finish
+          </button>
+          </div>
+          <ButtonBar message={result?.message} />
+        </form>
       </Card>
     </>
   );
