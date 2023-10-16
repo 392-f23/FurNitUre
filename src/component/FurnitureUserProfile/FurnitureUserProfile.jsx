24d7b6493cea7c678ced7247d847f0476098f8c8
DiffatElicka <903869629@qq.com>
2023-10-16 13:55:23 -0500

    Better Temp User Profile Page

--- src/component/FurnitureUserProfile/FurnitureUserProfile.jsx
+++ src/component/FurnitureUserProfile/FurnitureUserProfile.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import './FurnitureUserProfile.less'
 
 export const firstLastName = (user) => (
   user ? `${user.firstName} ${user.lastName}` : 'Unknown user'
@@ -7,18 +8,24 @@ export const firstLastName = (user) => (
 const FurnitureUserProfile   = ({profile}) => {
     console.log(profile);
     const user = profile.user;
-    return <div>
-    <h3>
-      { user.displayName}  
-      { 
-        profile?.isAdmin && 
-        <Link to={`/users/${user.id}/edit`} style={{fontSize: '0.7em', marginLeft: '2em'}}>
-          <i className="bi bi-pencil"></i>
-        </Link> }
-      </h3>
-    <p>Email: {user.email}</p>
-    <p>Phone: {user.phone}</p>
-  </div>
+//     return <div>
+//     <h3>
+//       { user.displayName}  
+//       { 
+//         profile?.isAdmin && 
+//         <Link to={`/users/${user.id}/edit`} style={{fontSize: '0.7em', marginLeft: '2em'}}>
+//           <i className="bi bi-pencil"></i>
+//         </Link> }
+//       </h3>
+//     <p>Email: {user.email}</p>
+//     <p>Phone: {user.phone}</p>
+//   </div>
+    return <div className="profile-card">
+        <img className="avatar-profile" src={`${user.photoURL}`} alt="User Avatar" />
+        <h2 className="user-name">{user.displayName}</h2>
+        <p className="user-email">{user.email}</p>
+        <p className="user-phone">{user.phone}</p>
+    </div>
 }
 
 export default FurnitureUserProfile;
