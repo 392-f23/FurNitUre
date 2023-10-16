3158beb18c86991949d49eea693e5df23102382c
DiffatElicka <903869629@qq.com>
2023-10-16 15:16:56 -0500

    Display fix + sign out fix

--- src/component/FunritureUserNav/FunritureUserNav.jsx
+++ src/component/FunritureUserNav/FunritureUserNav.jsx
@@ -17,12 +17,32 @@ const AuthButton = () => {
 
 const activation = ({isActive}) => isActive ? 'active' : 'inactive';
 
+
 const FunritureUserNav = ({profile}) => {
+  var avatar =  '';
+
+  if( profile.user != null){
+    avatar = profile.user.photoURL;
+  }
+
+  if(avatar == null){
+    avatar = '';
+  }
+
+  const AvatarButton = ({profile}) => {
+    if (profile == null){
+      return;
+    }
+    if (profile.user == null){
+      return;
+    }
+    return <NavLink to={`/users/${profile.user.uid}`} className={activation} end> <img src= {profile.user.photoURL} className='bg-info rounded-circle avatar'/></NavLink>
+  } 
 
   return <nav className="d-flex">
-    <NavLink to="/" className={activation} end><button className="ms-auto btn btn-dark">Furnitures</button></NavLink>
+    <NavLink to="/" className={activation} end><button className="ms-auto btn btn-dark sign-button">Furnitures</button></NavLink>
     <AuthButton />
-    <NavLink to={`/users/${profile.user.uid}`} className={activation} end> <img src= {profile.user.photoURL} className='bg-info rounded-circle avatar'/></NavLink>
+    <AvatarButton profile={profile}/>
   </nav>
 }
 
