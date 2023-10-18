c309434ce5121d000629b9b23c8f46ee5da49413
Elaine <jwubv@connect.ust.hk>
2023-10-18 14:10:56 -0500

    feat: add name searching

--- src/component/FurnitureContent/FurnitureContent.jsx
+++ src/component/FurnitureContent/FurnitureContent.jsx
@@ -1,13 +1,27 @@
 import FurnitureSideBar from "../FurnitureSideBar/FurnitureSideBar";
 import FurnitureItemContainer from "../FurnitureItemContainer/FurnitureItemContainer";
 import "./FurnitureContent.less";
+import { useState } from "react";
 
 const FurnitureContent = (props) => {
   const {data} = props
+  const [filteredItems, setFilteredItems] = useState(data.items);
+
+  const handleSearch = (query) => {
+    if (!query) {
+      setFilteredItems(data.items);
+      return;
+    }
+    const itemsArray = Object.values(data.items);
+    const filtered = itemsArray.filter(item =>
+      item.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredItems(filtered);
+  };
   return (
     <div className="furniture-content">
-      <FurnitureSideBar />
-      <FurnitureItemContainer items={data.items} />
+      <FurnitureSideBar onSearch={handleSearch} />
+      <FurnitureItemContainer items={filteredItems} />
     </div>
   );
 };
