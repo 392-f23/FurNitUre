79e4d7c5e8426882eefc20f45e6f7f9bc4bf5c95
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2023-10-19 01:30:41 -0500

    working search and filter

--- src/component/FurnitureItemContainer/FurnitureItemContainer.jsx
+++ src/component/FurnitureItemContainer/FurnitureItemContainer.jsx
@@ -1,12 +1,29 @@
 import FurnitureItem from "../FurnitureItem/FurnitureItem";
 import "./FurnitureItemContainer.less";
 
-const FurnitureItemContainer = (props) => {
-  const items = props.items;
+const FurnitureItemContainer = ({ items, filteredItems, filters }) => {
+  
+  const filterItems = (items) => {
+    if (filters.length === 0) return items;
+    
+    return items.filter(({ item }) => {
+      return filters.some(filterValue => {
+        return Object.values(item).includes(filterValue);
+      });
+    });
+  }
+
+  const itemsToDisplay = filteredItems
+    ? filterItems(filteredItems.map((fItem) => ({
+        id: fItem.id,
+        item: items[fItem.id],
+      })))
+    : filterItems(Object.entries(items).map(([id, item]) => ({ id, item })));
+
   return (
     <div className="furniture-item-container">
       <div className="furniture-item-list">
-        {Object.entries(items).map(([id, item]) => (
+        {itemsToDisplay.map(({ id, item }) => (
           <FurnitureItem key={id} itemId={id} item={item} />
         ))}
       </div>
--- src/component/FurnitureSideBar/FurnitureSideBar.jsx
+++ src/component/FurnitureSideBar/FurnitureSideBar.jsx
@@ -9,33 +9,31 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Button from "@mui/material/Button";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-
 import "./FurnitureSideBar.less";
 
-const FurnitureSideBar = (props) => {
+const FurnitureSideBar = ({ onSearch, onFilter }) => {
   const navigate = useNavigate();
   const createListingHandler = (e) => {
     e.stopPropagation();
     navigate(`/createListing`);
   };
 
-  const { onSearch } = props;
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
-    onSearch(query); 
+    onSearch(query);
   };
 
   return (
     <div className="furniture-sidebar">
       <div className="furniture-sidebar-search-container">
         <TextField
-              value={searchQuery} 
-              onChange={handleSearchChange} 
+          value={searchQuery}
+          onChange={handleSearchChange}
           sx={{
             fieldset: { border: "2px solid black" },
           }}
@@ -66,9 +64,21 @@ const FurnitureSideBar = (props) => {
           </AccordionSummary>
           <AccordionDetails className="furniture-sidebar-accrodin-content">
             <div className="furniture-sidebar-accrodin-checkbox-container">
-              <FormControlLabel control={<Checkbox />} label="All" />
-              <FormControlLabel control={<Checkbox />} label="Local pickup" />
-              <FormControlLabel control={<Checkbox />} label="Shipping" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="All"
+                onChange={() => onFilter("All")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Local pickup"
+                onChange={() => onFilter("Local pickup")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Shipping"
+                onChange={() => onFilter("Shipping")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -83,13 +93,26 @@ const FurnitureSideBar = (props) => {
           </AccordionSummary>
           <AccordionDetails className="furniture-sidebar-accrodin-content">
             <div className="furniture-sidebar-accrodin-checkbox-container">
-              <FormControlLabel control={<Checkbox />} label="New" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="New"
+                onChange={() => onFilter("New")}
+              />
               <FormControlLabel
                 control={<Checkbox />}
                 label="Used - Like new"
+                onChange={() => onFilter("Used - Like new")}
               />
-              <FormControlLabel control={<Checkbox />} label="Used - Good" />
-              <FormControlLabel control={<Checkbox />} label="Used - Fair" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Used - Good"
+                onChange={() => onFilter("Used - Good")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Used - Fair"
+                onChange={() => onFilter("Used - Fair")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -107,18 +130,33 @@ const FurnitureSideBar = (props) => {
               <FormControlLabel
                 control={<Checkbox />}
                 label="Ballard Designs"
+                onChange={() => onFilter("Ballard Designs")}
               />
               <FormControlLabel
                 control={<Checkbox />}
                 label="Bernhardt Furniture"
+                onChange={() => onFilter("Bernhardt Furniture")}
               />
-              <FormControlLabel control={<Checkbox />} label="Ethan Allen" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Ethan Allen"
+                onChange={() => onFilter("Ethan Allen")}
+              />
               <FormControlLabel
                 control={<Checkbox />}
                 label="Restoration Hardware"
+                onChange={() => onFilter("Restoration Hardware")}
               />
-              <FormControlLabel control={<Checkbox />} label="Serena & Lily" />
-              <FormControlLabel control={<Checkbox />} label="Z Gallerie" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Serena & Lily"
+                onChange={() => onFilter("Serena & Lily")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Z Gallerie"
+                onChange={() => onFilter("Z Gallerie")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -136,22 +174,53 @@ const FurnitureSideBar = (props) => {
               <FormControlLabel
                 control={<Checkbox />}
                 label="Mid-Century Modern"
+                onChange={() => onFilter("Mid-Century Modern")}
               />
-              <FormControlLabel control={<Checkbox />} label="Industrial" />
-              <FormControlLabel control={<Checkbox />} label="Scandinavian" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Industrial"
+                onChange={() => onFilter("Industrial")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Scandinavian"
+                onChange={() => onFilter("Scandinavian")}
+              />
               <FormControlLabel
                 control={<Checkbox />}
                 label="Bohemian (Boho)"
+                onChange={() => onFilter("Bohemian (Boho)")}
               />
-              <FormControlLabel control={<Checkbox />} label="Farmhouse" />
-              <FormControlLabel control={<Checkbox />} label="Shabby Chic" />
-              <FormControlLabel control={<Checkbox />} label="Art Deco" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Farmhouse"
+                onChange={() => onFilter("Farmhouse")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Shabby Chic"
+                onChange={() => onFilter("Shabby Chic")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Art Deco"
+                onChange={() => onFilter("Art Deco")}
+              />
               <FormControlLabel
                 control={<Checkbox />}
                 label="Coastal/Hamptons"
+                onChange={() => onFilter("Coastal/Hamptons")}
               />
-              <FormControlLabel control={<Checkbox />} label="Traditiona" />
-              <FormControlLabel control={<Checkbox />} label="Contemporary" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Traditiona"
+                onChange={() => onFilter("Traditiona")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Contemporary"
+                onChange={() => onFilter("Contemporary")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -166,10 +235,26 @@ const FurnitureSideBar = (props) => {
           </AccordionSummary>
           <AccordionDetails className="furniture-sidebar-accrodin-content">
             <div className="furniture-sidebar-accrodin-checkbox-container">
-              <FormControlLabel control={<Checkbox />} label="Distressed" />
-              <FormControlLabel control={<Checkbox />} label="Mirrored" />
-              <FormControlLabel control={<Checkbox />} label="Painted" />
-              <FormControlLabel control={<Checkbox />} label="Stained" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Distressed"
+                onChange={() => onFilter("Distressed")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Mirrored"
+                onChange={() => onFilter("Mirrored")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Painted"
+                onChange={() => onFilter("Painted")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Stained"
+                onChange={() => onFilter("Stained")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -184,9 +269,21 @@ const FurnitureSideBar = (props) => {
           </AccordionSummary>
           <AccordionDetails className="furniture-sidebar-accrodin-content">
             <div className="furniture-sidebar-accrodin-checkbox-container">
-              <FormControlLabel control={<Checkbox />} label="Fabric" />
-              <FormControlLabel control={<Checkbox />} label="Leather" />
-              <FormControlLabel control={<Checkbox />} label="Metal" />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Fabric"
+                onChange={() => onFilter("Fabric")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Leather"
+                onChange={() => onFilter("Leather")}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Metal"
+                onChange={() => onFilter("Metal")}
+              />
             </div>
           </AccordionDetails>
         </Accordion>
@@ -197,7 +294,7 @@ const FurnitureSideBar = (props) => {
           size="small"
           className="furniture-sidebar-publish-furniture-button"
           disableRipple={true}
-          onClick = {createListingHandler}
+          onClick={createListingHandler}
         >
           Publish Furniture
         </Button>
