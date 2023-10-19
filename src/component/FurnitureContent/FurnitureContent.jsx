964c2b58c40f18f9642c57e7421d380b8edb07a3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-10-19 01:31:12 -0500

    working search and filter (#20)

--- src/component/FurnitureContent/FurnitureContent.jsx
+++ src/component/FurnitureContent/FurnitureContent.jsx
@@ -4,26 +4,42 @@ import "./FurnitureContent.less";
 import { useState } from "react";
 
 const FurnitureContent = (props) => {
-  const {data} = props
-  const [filteredItems, setFilteredItems] = useState(data.items);
+  const { data } = props;
+  const [filters, setFilters] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(null);
 
   const handleSearch = (query) => {
     if (!query) {
-      setFilteredItems(data.items);
+      setFilteredItems(null);
       return;
     }
-    const itemsArray = Object.values(data.items);
-    const filtered = itemsArray.filter(item =>
-      item.furnitureName.toLowerCase().includes(query.toLowerCase()) 
-      || 
-      item.description.toLowerCase().includes(query.toLowerCase())
-    );
+    const itemsArray = Object.entries(data.items);
+    const filtered = itemsArray
+      .filter(
+        ([id, item]) =>
+          item.furnitureName.toLowerCase().includes(query.toLowerCase()) ||
+          item.description.toLowerCase().includes(query.toLowerCase())
+      )
+      .map(([id, item]) => ({ ...item, id }));
     setFilteredItems(filtered);
   };
+
+  const handleFilter = (filterType) => {
+    if (filters.includes(filterType)) {
+      setFilters((prevFilters) => prevFilters.filter((f) => f !== filterType));
+    } else {
+      setFilters((prevFilters) => [...prevFilters, filterType]);
+    }
+  };
+
   return (
     <div className="furniture-content">
-      <FurnitureSideBar onSearch={handleSearch} />
-      <FurnitureItemContainer items={filteredItems} />
+      <FurnitureSideBar onSearch={handleSearch} onFilter={handleFilter} />
+      <FurnitureItemContainer
+        items={data.items}
+        filteredItems={filteredItems}
+        filters={filters}
+      />
     </div>
   );
 };
