7a68a4f5f5231bd5faa61708bb5f955d29cb2511
DiffatElicka <37790161+DiffatElicka@users.noreply.github.com>
2023-10-19 21:26:33 -0500

    Chenyu/user profile (#17)
    * Functional Signin and out with basic user profile
    
    * Better Temp User Profile Page
    
    * Display fix + sign out fix
    
    * User Profile Page Item Container
    
    * update
    
    ---------
    
    Co-authored-by: Hongda <67817916+Hongda-OSU@users.noreply.github.com>

---
+++ src/component/FunritureUserNav/FunritureUserNav.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from 'react-router-dom';
+import { signInWithGoogle, signOut, useAuthState } from '../../utilities/firebase.js';
+import './FunritureUserNav.less'
+
+const SignInButton = () => (
+  <button className="ms-auto btn btn-dark" onClick={signInWithGoogle}>Sign in</button>
+);
+
+const SignOutButton = () => (
+  <button className="ms-auto btn btn-dark" onClick={signOut}>Sign out</button>
+);
+
+const AuthButton = () => {
+  const [user] = useAuthState();
+  return user ? <SignOutButton /> : <SignInButton />;
+};
+
+const activation = ({isActive}) => isActive ? 'active' : 'inactive';
+
+
+const FunritureUserNav = ({profile}) => {
+  var avatar =  '';
+
+  if( profile.user != null){
+    avatar = profile.user.photoURL;
+  }
+
+  if(avatar == null){
+    avatar = '';
+  }
+
+  const AvatarButton = ({profile}) => {
+    if (profile == null){
+      return;
+    }
+    if (profile.user == null){
+      return;
+    }
+    return <NavLink to={`/users/${profile.user.uid}`} className={activation} end> <img src= {profile.user.photoURL} className='bg-info rounded-circle avatar'/></NavLink>
+  } 
+
+  return <nav className="d-flex">
+    <AuthButton />
+    <AvatarButton profile={profile}/>
+  </nav>
+}
+
+
+export default FunritureUserNav;
---
+++ src/component/FurnitureUserProfile/FurnitureUserProfile.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import './FurnitureUserProfile.less'
+import FurnitureItemContainer from '../FurnitureItemContainer/FurnitureItemContainer'
+
+export const findUserItems = (user, data) => {
+    const name = user.displayName;
+    const owned = data.items.filter((item) => {
+        item.sellerName == name;
+    })
+    return owned;
+}
+
+const FurnitureUserProfile = ({profile, data}) => {
+    console.log(data);
+    const user = profile.user;
+    const owned = findUserItems(user, data);
+    console.log(owned);
+//     return <div>
+//     <h3>
+//       { user.displayName}  
+//       { 
+//         profile?.isAdmin && 
+//         <Link to={`/users/${user.id}/edit`} style={{fontSize: '0.7em', marginLeft: '2em'}}>
+//           <i className="bi bi-pencil"></i>
+//         </Link> }
+//       </h3>
+//     <p>Email: {user.email}</p>
+//     <p>Phone: {user.phone}</p>
+//   </div>
+    return <div className="profile-card">
+        <img className="avatar-profile" src={`${user.photoURL}`} alt="User Avatar" />
+        <h2 className="user-name">{user.displayName}</h2>
+        <p className="user-email">{user.email}</p>
+        <p className="user-phone">{user.phone}</p>
+        <FurnitureItemContainer items={data.items} />
+    </div>
+}
+
+export default FurnitureUserProfile;
