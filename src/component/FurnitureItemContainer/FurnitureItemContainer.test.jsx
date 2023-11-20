b2e3ef43b13e0d8aacf8240bcc5553faf3f9d7a9
adrianhoffer35@gmail.com <adrianhoffer35@gmail.com>
2023-11-20 13:02:47 -0600

    hard test

--- src/component/FurnitureItemContainer/FurnitureItemContainer.test.jsx
+++ src/component/FurnitureItemContainer/FurnitureItemContainer.test.jsx
@@ -1,3 +1,38 @@
-import { render, fireEvent, screen } from "@testing-library/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { render, screen } from '@testing-library/react';
+import FurnitureItemContainer from './FurnitureItemContainer';
+import { BrowserRouter } from 'react-router-dom';
+import FurnitureItem from '../FurnitureItem/FurnitureItem';
+
+vi.mock('../FurnitureItem/FurnitureItem', () => {
+    return {
+        default: ({ item }) => (
+          <div data-testid="furniture-item">
+            <div className="furniture-item-name">{item.furnitureName}</div>
+            {/* Add other elements as needed */}
+          </div>
+        )
+      };
+  });
+  
+
+describe('FurnitureItemContainer Tests', () => {
+  it('displays only items that meet the applied filter', async () => {
+    const items = {
+      '1': { furnitureName: 'Chair', condition: 'New' },
+      '2': { furnitureName: 'Table', condition: 'Used' },
+      '3': { furnitureName: 'Sofa', condition: 'New' }
+    };
+    const filteredItems = [{ id: '1' }, { id: '3' }];
+    const filters = ['New'];
+    render(
+        <BrowserRouter>
+          <FurnitureItemContainer items={items} filteredItems={filteredItems} filters={filters} />
+        </BrowserRouter>
+      );
+      const chairItem = await screen.findByText('Chair');
+      const sofaItem = await screen.findByText('Sofa');
+  
+      expect(document.body.contains(chairItem)).to.be.true;
+      expect(document.body.contains(sofaItem)).to.be.true;
+  });
+});
